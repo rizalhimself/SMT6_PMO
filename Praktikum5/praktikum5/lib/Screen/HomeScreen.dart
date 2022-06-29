80a152f1264080c4c7223c61c4bd1a9d1d67637d
rizalhimself <rizalhimself@gmail.com>
2022-06-29 11:37:55 +0700

    Finished Praktikum 5

--- Praktikum5/praktikum5/lib/Screen/HomeScreen.dart
+++ Praktikum5/praktikum5/lib/Screen/HomeScreen.dart
@@ -21,6 +21,10 @@ class Home extends StatefulWidget {
 int _selectedIndex = 0;
 
 class _HomeState extends State<Home> {
+  TextEditingController controllerNama = TextEditingController();
+  TextEditingController controllerAlamat = TextEditingController();
+  TextEditingController controllerTempatLahir = TextEditingController();
+
   void _itemDiTap(int index) {
     setState(() {
       _selectedIndex = index;
@@ -53,10 +57,13 @@ class _HomeState extends State<Home> {
                       },
                       child: const Text("OK")),
                 ),
+                Text("Nama Lengkap : " + controllerNama.text),
+                Text("Alamat : " + controllerAlamat.text),
+                Text("Tempat Lahr : " + controllerTempatLahir.text),
                 Text("Jenis Kelamin : " + jenisKelaminDipilih),
                 Text("Status : " + statusPernikahanDipilih),
+                Text("Agama : " + agamaDipilih),
                 Text("Kemampuan Berbahasa : " + cariBahasa()),
-                Text("Agama : " + agamaDipilih)
               ],
             )
           ],
@@ -72,7 +79,16 @@ class _HomeState extends State<Home> {
     );
   }
 
-  void _kosongkan() {}
+  void _kosongkan() {
+    controllerNama.text = "";
+    controllerAlamat.text = "";
+    controllerAlamat.text = "";
+    jenisKelaminDipilih = "";
+    statusPernikahanDipilih = "";
+    agamaDipilih = "";
+    chkArab = chkIndonesia = chkInggris = chkJawa = chkJepang = false;
+    chkKorea = chkMadura = chkMandarin = chkSunda = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -89,6 +105,22 @@ class _HomeState extends State<Home> {
               padding: const EdgeInsets.all(10.1),
               child: Column(
                 children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                        hintText: "Nama Lengkap", labelText: "Nama Lengkap"),
+                    controller: controllerNama,
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(
+                        hintText: "Alamat", labelText: "Alamat"),
+                    controller: controllerAlamat,
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(
+                        hintText: "Tempat Lahir", labelText: "Tempat Lahir"),
+                    controller: controllerTempatLahir,
+                  ),
+                  Pemisah(),
                   JenisKelamin(),
                   Pemisah(),
                   StatusPernikahan(),
